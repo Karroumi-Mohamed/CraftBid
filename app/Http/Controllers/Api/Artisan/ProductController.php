390d0bb90ecdadc3e9c03972557df30d3c33da27
Mohamed Karroumi <mkaroumi123@gmail.com>
2025-04-25 00:36:04 +0100

    Add ProductController with placeholder methods for CRUD operations

---
+++ app/Http/Controllers/Api/Artisan/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\Artisan;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        //TODO
+    }
+
+    public function store(Request $request)
+    {
+        //TODO
+    }
+
+    public function show(string $id)
+    {
+        //TODO
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //TODO
+    }
+
+    public function destroy(string $id)
+    {
+        //TODO
+    }
+}
