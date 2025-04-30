301728edfea8fa937ac787a27154b4626617af7f
mohamed karroumi <mkaroumi123@gmail.com>
2025-04-30 11:30:02 +0100

    Add Setting model and migration for settings table creation

---
+++ app/Models/Setting.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true; // Enable timestamps
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+}
