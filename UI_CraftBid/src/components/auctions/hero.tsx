f77aaf5749806f3b070cfe0b4324e9feffb87c62
mohamed karroumi <mkaroumi123@gmail.com>
2025-05-02 14:30:20 +0100

    Enhance Hero component: Add descriptive text and a call-to-action button for improved user engagement.

--- UI_CraftBid/src/components/auctions/hero.tsx
+++ UI_CraftBid/src/components/auctions/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
     return (
         <div className="min-h-[70vh] px-28 flex items-center justify-center overflow-hidden">
             <div>
-                <h1 className={`text-6xl font-montserrat font-semibold leading-20 transition-opacity duration-700 ease-in-out ${isMounted ? 'opacity-100' : 'opacity-0'}`}>
+                <h1 className={`text-6xl font-montserrat font-semibold leading-20 transition-opacity duration-700 ease-in-out ${isMounted ? 'opacity-100' : 'opacity-0'} mb-4`}>
                     <span className={`relative inline-block transition-all transform duration-700 ease-in-out ${isMounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
                         <span className="relative px-3 pr-4 inline-block">
                             <div className=" z-10 absolute w-full bg-green-700 h-[70px] top-1.5 left-0 rounded-2xl bg-[url('/hero1.png')] bg-left"></div>
@@ -38,6 +38,12 @@ export default function Hero() {
                     }
                     >Morocco. </span>
                 </h1>
+                <p className={`mt-6 text-gray-700 max-w-md transition-all duration-700 ease-in-out delay-300 font-montserrat font-semibold ${isMounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..
+                </p>
+                <button className={`font-montserrat font-semibold mt-8 px-6 py-3 bg-black text-white rounded-full flex items-center gap-2 transition-all duration-700 ease-in-out delay-400 ${isMounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'} hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2`}>
+                    start biding <span className="text-xl">→</span>
+                </button>
             </div>
             <div className='flex shrink-0'>
                 <img
