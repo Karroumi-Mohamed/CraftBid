a22c004fba1c2eb14ca7f96da0e79e8aa2816946
mohamed karroumi <mkaroumi123@gmail.com>
2025-05-02 09:59:41 +0100

    integrating a Navbar component. Adjust the Auction interface for better structure and maintainability.

--- UI_CraftBid/src/pages/AuctionDetailPage.tsx
+++ UI_CraftBid/src/pages/AuctionDetailPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import api, { makeRequest } from '../lib/axois';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
